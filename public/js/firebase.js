0a2fa9f7185003d5c8151f17f33f9047487f216c
Biswajit-13 <107855485+Biswajit-13@users.noreply.github.com>
2022-12-03 12:06:52 +0530

    first commit

---
+++ public/js/firebase.js
@@ -0,0 +1,12 @@
+let firebaseConfig = {
+
+    apiKey: "AIzaSyC1J2EiIu3jAIgMq3tmTdjzaXMCtMC5RQA",
+    authDomain: "blog-19936.firebaseapp.com",
+    projectId: "blog-19936",
+    storageBucket: "blog-19936.appspot.com",
+    messagingSenderId: "742817120122",
+    appId: "1:742817120122:web:f264f87226c05c5edb20c3"};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
